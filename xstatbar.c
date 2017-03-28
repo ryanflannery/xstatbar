d4c967dc0e274c992699043a8070d56ae643a9f4
Wes <me@wmoxam.com>
2017-03-28 00:59:50 -0400

    Pull colors from X resources

--- xstatbar.c
+++ xstatbar.c
@@ -145,7 +145,7 @@ main (int argc, char *argv[])
 
       /* draw */
       draw(consolidate_cpus);
-			XSync(XINFO.disp, False);
+      XSync(XINFO.disp, False);
 
       /* sleep */
       sleep(sleep_seconds);
@@ -194,89 +194,136 @@ process_signals()
 void
 cleanup()
 {
-   /* x teardown */
-   XdbeDeallocateBackBufferName(XINFO.disp, XINFO.backbuf);
-   XrmDestroyDatabase(XINFO.xrdb);
-   XClearWindow(XINFO.disp,   XINFO.win);
-   XDestroyWindow(XINFO.disp, XINFO.win);
-   XftDrawDestroy( XINFO.xftdraw );
-	 XCloseDisplay(XINFO.disp);
+  /* x teardown */
+  XdbeDeallocateBackBufferName(XINFO.disp, XINFO.backbuf);
+  XrmDestroyDatabase(XINFO.xrdb);
+  XClearWindow(XINFO.disp,   XINFO.win);
+  XDestroyWindow(XINFO.disp, XINFO.win);
+  XftDrawDestroy( XINFO.xftdraw );
+  XCloseDisplay(XINFO.disp);
 
-   /* stats teardown */
-   volume_close();
-   power_close();
-   sysinfo_close();
+  XftColorFree(XINFO.disp, XINFO.vis, DefaultColormap( XINFO.disp, XINFO.screen ), &COLOR0);
+  XftColorFree(XINFO.disp, XINFO.vis, DefaultColormap( XINFO.disp, XINFO.screen ), &COLOR1);
+  XftColorFree(XINFO.disp, XINFO.vis, DefaultColormap( XINFO.disp, XINFO.screen ), &COLOR2);
+  XftColorFree(XINFO.disp, XINFO.vis, DefaultColormap( XINFO.disp, XINFO.screen ), &COLOR3);
+  XftColorFree(XINFO.disp, XINFO.vis, DefaultColormap( XINFO.disp, XINFO.screen ), &COLOR4);
+  XftColorFree(XINFO.disp, XINFO.vis, DefaultColormap( XINFO.disp, XINFO.screen ), &COLOR5);
+  XftColorFree(XINFO.disp, XINFO.vis, DefaultColormap( XINFO.disp, XINFO.screen ), &COLOR6);
+  XftColorFree(XINFO.disp, XINFO.vis, DefaultColormap( XINFO.disp, XINFO.screen ), &COLOR7);
 
-   exit(0);
+
+  /* stats teardown */
+  volume_close();
+  power_close();
+  sysinfo_close();
+
+  exit(0);
 }
 
 /* get resource from X Resource database */
 const char *
 get_resource(const char *resource)
 {
-	static char name[256], class[256], *type;
-	XrmValue value;
+  static char name[256], class[256], *type;
+  XrmValue value;
 
-	if (!XINFO.xrdb)
-		return NULL;
+  if (!XINFO.xrdb)
+    return NULL;
 #define RESCLASS "xstatbar"
 #define RESNAME "XStatBar"
-	snprintf(name, sizeof(name), "%s.%s", RESNAME, resource);
-	snprintf(class, sizeof(class), "%s.%s", RESCLASS, resource);
-	printf("Name: %s \n", name);
-	XrmGetResource(XINFO.xrdb, name, class, &type, &value);
-	if (value.addr)
-		return value.addr;
-	return NULL;
+  snprintf(name, sizeof(name), "%s.%s", RESNAME, resource);
+  snprintf(class, sizeof(class), "%s.%s", RESCLASS, resource);
+  XrmGetResource(XINFO.xrdb, name, class, &type, &value);
+  if (value.addr)
+    return value.addr;
+  return NULL;
 }
 
-XRenderColor *
-calc_color(const char *name, XRenderColor *def)
+XRenderColor*
+hex_to_color(const char *hex)
+{
+  if (strlen(hex) < 7) {
+    return NULL;
+  } 
+
+  XRenderColor *color = malloc(sizeof(XRenderColor));
+  long number = strtoll( &hex[1], NULL, 16);
+  long r = number >> 16;
+  long g = number >> 8 & 0xFF;
+  long b = number & 0xFF;
+
+
+  color->red = ((r + 1) * 64 * 16) - 1;
+  color->green = ((g + 1) * 64 * 16) - 1;
+  color->blue = ((b + 1) * 64 * 16) - 1;
+  color->alpha = 0xffff;
+
+  return color;
+}
+
+void
+calc_color(const char *name, XRenderColor *def, XftColor *col)
 {
   const char *color;
-	color = get_resource(name);
+  color = get_resource(name);
+  XftColor lookup_color;
+  XRenderColor *hex_color;
 
-	if (color) {
+  if (color) {
     if (color[0] == '#') {
-			printf("HEX COLOR\n");
-    } else {
-			printf("LOOKUP\n");
-		}
-	} else {
-    printf("No resource\n");
-	}
+      hex_color = hex_to_color(color);
+      if (XftColorAllocValue(XINFO.disp, 
+            XINFO.vis, 
+            DefaultColormap( XINFO.disp, XINFO.screen ), 
+            hex_color, 
+            &lookup_color) ) {
+        *col = lookup_color;
+        return;
+      }
+     } else {
+      if (XftColorAllocName(XINFO.disp, 
+            XINFO.vis, 
+            DefaultColormap( XINFO.disp, XINFO.screen ), 
+            color, 
+            &lookup_color) ) {
+        *col = lookup_color;
+        return;
+      } else {
+      }
+    }
+  }
 
-	return def;
+  XftColorAllocValue(XINFO.disp, XINFO.vis, DefaultColormap( XINFO.disp, XINFO.screen ), def, col);
 }
 
 /* setup all colors used */
 void
 setup_colors()
 {
-  XRenderColor white   = { .red = 0xffff, .green = 0xffff,	.blue = 0xffff, .alpha = 0xffff };
-  XRenderColor red     = { .red = 0xffff, .green = 0x0,			.blue = 0x0,		.alpha = 0xffff };
-  XRenderColor green   = { .red = 0x0,    .green = 0xf000,	.blue = 0x0,		.alpha = 0xffff };
-  XRenderColor blue    = { .red = 0x0,    .green = 0x0,			.blue = 0xffff, .alpha = 0xffff };
-  XRenderColor yellow  = { .red = 0xffff, .green = 0xffff,	.blue = 0x0,		.alpha = 0xffff };
-  XRenderColor magenta = { .red = 0xffff, .green = 0x0,			.blue = 0xffff,	.alpha = 0xffff };
-  XRenderColor cyan    = { .red = 0x0,    .green = 0xffff,	.blue = 0xffff,	.alpha = 0xffff };
-  XRenderColor black   = { .red = 0x0,    .green = 0x0,			.blue = 0x0,		.alpha = 0xaaaa };
+  XRenderColor color0  = { .red = 0x0,    .green = 0x0,    .blue = 0x0,    .alpha = 0xffff };
+  XRenderColor color1  = { .red = 0xffff, .green = 0x0,    .blue = 0x0,    .alpha = 0xffff };
+  XRenderColor color2  = { .red = 0x0,    .green = 0xf000, .blue = 0x0,    .alpha = 0xffff };
+  XRenderColor color3  = { .red = 0xffff, .green = 0xffff, .blue = 0x0,    .alpha = 0xffff };
+  XRenderColor color4  = { .red = 0x0,    .green = 0x0,    .blue = 0xffff, .alpha = 0xffff };
+  XRenderColor color5  = { .red = 0xffff, .green = 0x0,    .blue = 0xffff, .alpha = 0xffff };
+  XRenderColor color6  = { .red = 0x0,    .green = 0xffff, .blue = 0xffff, .alpha = 0xffff };
+  XRenderColor color7  = { .red = 0xffff, .green = 0xffff, .blue = 0xffff, .alpha = 0xffff };
 
-	XftColorAllocValue(XINFO.disp, XINFO.vis, DefaultColormap( XINFO.disp, XINFO.screen ), calc_color("color0", &black),   &COLOR0);
-	XftColorAllocValue(XINFO.disp, XINFO.vis, DefaultColormap( XINFO.disp, XINFO.screen ), &red,     &COLOR1);
-	XftColorAllocValue(XINFO.disp, XINFO.vis, DefaultColormap( XINFO.disp, XINFO.screen ), &green,   &COLOR2);
-	XftColorAllocValue(XINFO.disp, XINFO.vis, DefaultColormap( XINFO.disp, XINFO.screen ), &yellow,  &COLOR3);
-	XftColorAllocValue(XINFO.disp, XINFO.vis, DefaultColormap( XINFO.disp, XINFO.screen ), &blue,    &COLOR4);
-	XftColorAllocValue(XINFO.disp, XINFO.vis, DefaultColormap( XINFO.disp, XINFO.screen ), &magenta, &COLOR5);
-	XftColorAllocValue(XINFO.disp, XINFO.vis, DefaultColormap( XINFO.disp, XINFO.screen ), &cyan,    &COLOR6);
-	XftColorAllocValue(XINFO.disp, XINFO.vis, DefaultColormap( XINFO.disp, XINFO.screen ), &white,   &COLOR7);
+  calc_color("color0", &color0, &COLOR0);
+  calc_color("color1", &color1, &COLOR1);
+  calc_color("color2", &color2, &COLOR2);
+  calc_color("color3", &color3, &COLOR3);
+  calc_color("color4", &color4, &COLOR4);
+  calc_color("color5", &color5, &COLOR5);
+  calc_color("color6", &color6, &COLOR6);
+  calc_color("color7", &color7, &COLOR7);
 }
 
 int
 calculate_width_of_default_screen()
 {
-	int nsizes;
-	XRRScreenSize* randrsize = XRRSizes(XINFO.disp, XINFO.screen, &nsizes);
+  int nsizes;
+  XRRScreenSize* randrsize = XRRSizes(XINFO.disp, XINFO.screen, &nsizes);
 
   if (nsizes != 0) {
     Rotation current = 0;
@@ -292,7 +339,7 @@ calculate_width_of_default_screen()
 
       bool rot = current & RR_Rotate_90 || current & RR_Rotate_270;
       return rot ? randrsize->height : randrsize->width;
-		}
+    }
   }
   return DisplayWidth(XINFO.disp, XINFO.screen);
 }
@@ -317,14 +364,12 @@ setup_x(int x, int y, int w, int h, const char *font)
    XINFO.depth  = DefaultDepth(XINFO.disp, XINFO.screen);
    XINFO.vis    = DefaultVisual(XINFO.disp, XINFO.screen);
    XINFO.width  = w ? w : calculate_width_of_default_screen();
-	 x11_window_attributes.override_redirect = 1;
+   x11_window_attributes.override_redirect = 1;
 
    if(!(XINFO.xrdb = XrmGetDatabase(XINFO.disp))) {
       xrms = XResourceManagerString(XINFO.disp);
       if (xrms)
          XINFO.xrdb = XrmGetStringDatabase(xrms);
-			else
-				printf("No xrdb\n");
 
    }
 
@@ -338,26 +383,26 @@ setup_x(int x, int y, int w, int h, const char *font)
       CWOverrideRedirect, &x11_window_attributes
    );
 
-	 /* setup window manager hints */
+   /* setup window manager hints */
    type = XInternAtom(XINFO.disp, "_NET_WM_WINDOW_TYPE_DOCK", False);
    XChangeProperty(XINFO.disp, XINFO.win, XInternAtom(XINFO.disp, "_NET_WM_WINDOW_TYPE", False),
-		   XA_ATOM, 32, PropModeReplace, (unsigned char*)&type, 1);
+       XA_ATOM, 32, PropModeReplace, (unsigned char*)&type, 1);
    bzero(struts, sizeof(struts));
    enum { left, right, top, bottom, left_start_y, left_end_y, right_start_y,
-	  right_end_y, top_start_x, top_end_x, bottom_start_x, bottom_end_x };
+    right_end_y, top_start_x, top_end_x, bottom_start_x, bottom_end_x };
    if (y <= DisplayHeight(XINFO.disp, XINFO.screen)/2) {
-	   struts[top] = y + XINFO.height;
-	   struts[top_start_x] = x;
-	   struts[top_end_x] = x + XINFO.width;
+     struts[top] = y + XINFO.height;
+     struts[top_start_x] = x;
+     struts[top_end_x] = x + XINFO.width;
    } else {
-	   struts[bottom] = DisplayHeight(XINFO.disp, XINFO.screen) - y;
-	   struts[bottom_start_x] = x;
-	   struts[bottom_end_x] = x + XINFO.width;
+     struts[bottom] = DisplayHeight(XINFO.disp, XINFO.screen) - y;
+     struts[bottom_start_x] = x;
+     struts[bottom_end_x] = x + XINFO.width;
    }
    XChangeProperty(XINFO.disp, XINFO.win, XInternAtom(XINFO.disp, "_NET_WM_STRUT_PARTIAL", False),
-		   XA_CARDINAL, 32, PropModeReplace, (unsigned char*)struts, 12);
+       XA_CARDINAL, 32, PropModeReplace, (unsigned char*)struts, 12);
 
-	 XINFO.backbuf = XdbeAllocateBackBufferName(XINFO.disp, XINFO.win, XdbeBackground);
+   XINFO.backbuf = XdbeAllocateBackBufferName(XINFO.disp, XINFO.win, XdbeBackground);
    XINFO.xftdraw = XftDrawCreate(XINFO.disp, XINFO.backbuf,
                                  DefaultVisual(XINFO.disp,XINFO.screen),
                                  DefaultColormap( XINFO.disp, XINFO.screen ) );
